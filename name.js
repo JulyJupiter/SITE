6d838f396d29e43b6eb6689226f53744f8229850
JulyJupiter <et.juliyan.d@eduiteh.eu>
2025-11-20 13:16:32 +0200

    about me page - added

--- name.js
+++ name.js
@@ -7,5 +7,15 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     profileImg.addEventListener("mouseleave", () => {
     NameTag.style.opacity = 0;
+
+    const ImageAboutMe = document.querySelector("image-about-me");
+
+    ImageAboutMe.addEventListener("click", () =>
+    {
+        ImageAboutMe.addClass = "bounce-animation";
+        setTimeout(function() {
+            ImageAboutMe.removeClass("bounce-animation");
+        }, 500);
+    }); 
 });
 });
